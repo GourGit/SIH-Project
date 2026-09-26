f6a435d7e5d3bbd64ce3cd2d6adeb32d6e427dbc
Maheshwar Murmu <maheshwar2005murmu@gmail.com>
2026-09-26 12:16:41 +0530

    Update DB_PASSWORD
    Change DB_PASSWORD to retrieve from environment variable

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ class Config:
     
     # Database Configuration
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD='paylRoy054'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
     DB_PORT = os.environ.get('DB_PORT', '3306')
     DB_NAME = os.environ.get('DB_NAME', 'indian_climate')
